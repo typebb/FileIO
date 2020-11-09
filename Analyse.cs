f888801b0d45d35c68cd1c04b4231e6a169d14ae
Kjel Arens <kjelarens@hotmail.com>
2020-11-09 05:49:25 +0100

    Analyse klasse toegevoegd

---
+++ Analyse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIO
+{
+    public class Analyse
+    {
+        private AnalyseBestanden AnalyseBestanden { get; set; }
+        private AnalyseKlassen AnalyseKlassen { get; set; }
+        public Analyse()
+        {
+            Input input = new Input();
+            Output output = new Output();
+            AnalyseBestanden = new AnalyseBestanden();
+            AnalyseKlassen = new AnalyseKlassen();
+            AnalyseBestanden.Input = input;
+            AnalyseBestanden.Output = output;
+            AnalyseKlassen.Input = input;
+            AnalyseKlassen.Output = output;
+        }
+        public void VoerAnalyseUit()
+        {
+            AnalyseBestanden.IterateFoldersAndFiles();
+            AnalyseKlassen.IterateFoldersAndFiles();
+        }
+    }
+}
