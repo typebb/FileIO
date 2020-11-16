573b920346faf5a760e0cb3b23f2f1470e7cd174
Kjel Arens <kjelarens@hotmail.com>
2020-11-16 02:44:52 +0100

    update

--- AnalyseKlassen.cs
+++ AnalyseKlassen.cs
@@ -10,7 +10,7 @@ namespace FileIO
         public List<string> methodReturnTypes = new List<string> { "void", "bool", "byte", "char", "decimal", "double", "enum", "float", "int", "long", "sbyte", "short", "string", "struct", "uint", "ulong", "ushort" };
 
         public List<string> classDataTypes = new List<string>();
-        public char[] chars = new char[] { ':', ';', '{', '}', '=' };
+        public char[] chars = new char[] { ' ', ':', ';', '{', '}', '=' };
         public Input Input { get; set; } = new Input();
         public OutputClass Output { get; set; } = new OutputClass();
         public void IterateFoldersAndFiles()
@@ -50,13 +50,11 @@ namespace FileIO
                 {
                     nOpen++;
                     code = code.Substring(indexOpen + 1);
-                    CheckStringsAndAnalyse(code, output);
                 }
                 if (indexOpen > indexClose)
                 {
                     nOpen--;
                     code = code.Substring(indexClose + 1);
-                    CheckStringsAndAnalyse(code, output);
                 }
                 CheckStringsAndAnalyse(code, output);
             }
@@ -81,7 +79,7 @@ namespace FileIO
                 foreach(string u in code)
                 {
                     if (!u.Contains("namespace"))
-                        output.Usings.Add(u.Substring(s.IndexOf("using") + 6));
+                        output.Usings.Add(u.Substring(s.IndexOf("using") + 6).Trim());
                 }
             }
         }
@@ -137,13 +135,13 @@ namespace FileIO
         {
             if (s.Contains("(") && s.Contains(")"))
             {
-                if (s.Contains($"{output.Name}(") || s.Contains($"{output.Name} ("))
+                if (s.Contains($" {output.Name}(") || s.Contains($" {output.Name} ("))
                 {
                     var constructorNaam = s.Substring(s.IndexOf(output.Name)).Trim().Split(new char[] { ';', '=', '{', '}' }, StringSplitOptions.None);
                     foreach (string f in constructorNaam)
                     {
-                        if (f.Contains(")") && (f.Contains($"{output.Name}(") || f.Contains($"{output.Name} (")))
-                            output.Constructors.Add(f.Trim());
+                        if (f.Contains(")") && (f.Contains($" {output.Name}(") || f.Contains($" {output.Name} (")))
+                            output.Constructors.Add(f.Substring(f.IndexOf(output.Name)));
                     }
                 }
             }
@@ -159,8 +157,8 @@ namespace FileIO
                         var code = s.Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None);
                         foreach (string c in code)
                         {
-                            if (c.Contains(d) && (!c.Contains($"({d}") || !c.Contains($"( {d}")) && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (c.Contains("get") || c.Contains("set")))
-                                output.Properties.Add(c.Substring(c.IndexOf(d) + d.Length + 1).Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None)[0]);
+                            if (c.Contains($" {d} ") && !c.Contains($"( {d}") && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (c.Contains("get") || c.Contains("set")))
+                                output.Properties.Add(c.Substring(c.IndexOf(d) + d.Length + 1).Trim().Split(new char[] { ':', ';', '=', '{', '}', ')' }, StringSplitOptions.None)[0]);
                         }
                     }
                 }
@@ -171,8 +169,8 @@ namespace FileIO
                         var code = s.Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None);
                         foreach (string c in code)
                         {
-                            if (c.Contains(d) && (!c.Contains($"({d}") || !c.Contains($"( {d}")) && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (c.Contains("get") || c.Contains("set")))
-                                output.Properties.Add(c.Substring(c.IndexOf(d) + d.Length + 1).Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None)[0]);
+                            if (c.Contains($" {d} ") && !c.Contains($"( {d}") && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (c.Contains("get") || c.Contains("set")))
+                                output.Properties.Add(c.Substring(c.IndexOf(d) + d.Length + 1).Trim().Split(new char[] { ':', ';', '=', '{', '}', ')' }, StringSplitOptions.None)[0]);
                         }
                     }
                 }
@@ -184,10 +182,10 @@ namespace FileIO
             {
                 if (s.Contains(d))
                 {
-                    var code = s.Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None);
+                    var code = s.Trim().Split(new char[] { ':', ';', '}' }, StringSplitOptions.None);
                     foreach(string c in code)
                     {
-                        if (c.Contains(d) && (!c.Contains($"({d}") || !c.Contains($"( {d}")) && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (!c.Contains("get") || !c.Contains("set")))
+                        if (c.Contains($" {d} ") && !c.Contains(">") && (c.Substring(c.IndexOf(d) + d.Length) != "=") && !c.Contains("{") && !c.Contains(")") && (!c.Contains($"({d}") || !c.Contains($"( {d}")) && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (!c.Contains("get") || !c.Contains("set")))
                             output.Variables.Add(c.Substring(c.IndexOf(d) + d.Length + 1).Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None)[0]);
                     }
                 }
@@ -196,11 +194,11 @@ namespace FileIO
             {
                 if (s.Contains(d))
                 {
-                    var code = s.Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None);
+                    var code = s.Trim().Split(new char[] { ':', ';', '}' }, StringSplitOptions.None);
                     foreach (string c in code)
                     {
-                        if (c.Contains(d) && !c.Contains("(") && (!c.Contains($"{d}(") || !c.Contains($"{d} (")) && !c.Contains(" class ") && (!c.Contains($"({d}") || !c.Contains($"( {d}")) && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (!c.Contains("get") || !c.Contains("set")))
-                            output.Variables.Add(c.Substring(c.IndexOf(d) + d.Length + 1).Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None)[0]);
+                        if (c.Contains($" {d} ") && !c.Contains(">") && (c.Substring(c.IndexOf(d) + d.Length) != "=") && !c.Contains("{") && !c.Contains(")") && (!c.Contains($"({d}") || !c.Contains($"( {d}")) && (!c.Contains($",{d}") || !c.Contains($", {d}")) && (!c.Contains("get") || !c.Contains("set")))
+                            output.Variables.Add(c.Substring(c.IndexOf(d) + d.Length).Trim().Split(new char[] { ':', ';', '=', '}', ')' }, StringSplitOptions.None)[0]);
                     }
                 }
             }
@@ -214,7 +212,7 @@ namespace FileIO
                     string code = File.ReadAllText(f).Replace('\r', ' ').Replace('\n', ' ');
                     if (code.Contains(" class ") && !code.Contains(" abstract "))
                     {
-                        classDataTypes.Add(code.Substring(code.IndexOf("class") + 6).Trim().Split(chars, StringSplitOptions.None)[0]);
+                        classDataTypes.Add(code.Substring(code.IndexOf(" class ") + 6).Trim().Split(chars, StringSplitOptions.None)[0]);
                     }
                 }
             }
